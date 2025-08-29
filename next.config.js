f49fc648ffcbf8be29ddc6ef7aeca8894debc165
Jawad Cheema <techside94@gmail.com>
2025-08-29 21:01:45 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  // optional: basePath agar aap subfolder me deploy kar rahe hain
+  // basePath: "",
+};
+
+module.exports = nextConfig;
